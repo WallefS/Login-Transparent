6c4883aee409c61aed8ef9e253538af21b608c82
wallef <wallefsousa1@gmail.com>
2019-03-13 11:48:05 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login Form Tranparente</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="login-box">
+
+		<h1>Login</h1>
+
+		<div class="textbox">
+			<i class="fa fa-user" aria-hidden="true"></i>
+			<input type="text" placeholder="Nome" name="" value="">	
+		</div>
+
+		<div class="textbox">
+			<i class="fa fa-lock" aria-hidden="true"></i>
+			<input type="password" placeholder="Senha" name="" value="">
+		</div>
+
+		<input type="button" class="btn" value="Sign in">
+
+	</body>
+	</html>
